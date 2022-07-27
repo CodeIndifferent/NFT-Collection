3f29400d90ddcb2265e31929b9236a3ced8d027a
CodeIndifferent <84685307+CodeIndifferent@users.noreply.github.com>
2022-07-27 11:53:13 -0400

    Initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xBF66C2322423e4dBc75723a4115B1B13817c16a7";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
